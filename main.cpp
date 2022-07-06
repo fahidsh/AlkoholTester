0bcc63adf6ee6bc6557129f1d692f80b4296cb35
fahidsh <fahidsh@hotmail.com>
2022-07-06 18:51:28 +0000

    Comma Zahlen in Mbed

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "mbed.h"
+
+float pi = 3.14f;
+/* Serial Verbindung an 9600, anstatt 115200 */
+int main()
+{
+    printf("Comman Zahlen in Mbed ausdrucken\n");
+    printf("------------------------------\n");
+    printf("bitte beachten Sie die datei: mbed_app.json\n");
+    printf("------------------------------\n");
+    printf("Wert von PI ist %.4f\n", pi);
+    printf("Wert von 17/3 ist %.4f\n", 17.0/3.0);
+
+    float rand_zahl = 0.0f;
+    
+    while (true) {
+        rand_zahl = rand()/3.14;
+        printf("Zufallzahl: %.4f\n", ((float)rand_zahl/3.14));
+        ThisThread::sleep_for(1000ms);
+    }
+
+}
+
